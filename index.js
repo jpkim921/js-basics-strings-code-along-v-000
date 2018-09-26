75364900eb823e50577560cc7f0b8a706716634f
Jayon Kim <jpkim921@gmail.com>
2018-09-26 02:55:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = "Paul"
-const welcomeMesage = "Welcome to Flatbook, " + currentUser
+const welcomeMesage = "Welcome to Flatbook, " + currentUser + "!"
